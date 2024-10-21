c46c993ed45f63f979e4f66e99711a8a8477202a
0xgrowth <sapientiakonzept@gmail.com>
2024-10-21 03:25:11 +0100

    first triangle

---
+++ t.js
@@ -0,0 +1,43 @@
+console.log("Here's an equilateral triangle")
+console.log("             ")
+console.log("             .             ")
+
+setTimeout(() => {
+    console.log("            ...             ")
+}, 4000)
+
+setTimeout(() => {
+    console.log("           .....             ")
+}, 6000)
+
+setTimeout(() => {
+    console.log("          .......             ")
+}, 8000)
+
+setTimeout(() => {
+    console.log("         .........             ")
+}, 10000)
+
+setTimeout(() => {
+    console.log("        ...........             ")
+}, 12000)
+
+setTimeout(() => {
+    console.log("       .............             ")
+}, 14000)
+
+setTimeout(() => {
+    console.log("      ...............             ")
+}, 16000)
+
+setTimeout(() => {
+    console.log("     .................             ")
+}, 18000)
+
+setTimeout(() => {
+    console.log("    ...................             ")
+}, 20000)
+
+setTimeout(() => {
+    console.log("                          ")
+}, 22000)
